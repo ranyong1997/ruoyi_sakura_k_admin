1066e708ffd09b9320f139d6a196786fb93fdbda
ranyong <ranyong1209@gmail.com>
2024-09-23 22:21:37 +0800

    ✨ Feat(New data query base page): 新增数据查询基础页面

--- src/views/tool/querydb/index.vue
+++ src/views/tool/querydb/index.vue
@@ -3,22 +3,41 @@
     <el-row>
       <el-col :span="24" class="card-box">
         <el-card style="height:  calc(100vh - 135px); margin: 0 auto; border:1px ;  border-radius: 20px">
-          <el-select
-              v-model="value"
-              clearable
-              placeholder="请选择数据源"
-              style="width: 240px"
-              size="small"
-          >
-            <el-option
-                v-for="item in options"
-                :key="item.value"
-                :label="item.label"
-                :value="item.value"
-            />
-          </el-select>
-          <br/>
-          <span>当前库：</span>
+          <div class="common-layout">
+            <el-select
+                v-model="value"
+                clearable
+                placeholder="请选择数据源"
+                style="width: 240px"
+                size="small"
+            >
+              <el-option
+                  v-for="item in options"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value"
+              />
+            </el-select>
+            <br/>
+            <span>当前库：</span>
+            <splitpanes :push-other-panes="false" style="height: calc(97vh - 135px);">
+              <pane size="15">
+                <span>
+                  1
+                </span>
+              </pane>
+              <pane>
+                <splitpanes :push-other-panes="false" horizontal>
+                  <pane size="45">
+                    <span>2</span>
+                  </pane>
+                  <pane>
+                    <span>3</span>
+                  </pane>
+                </splitpanes>
+              </pane>
+            </splitpanes>
+          </div>
         </el-card>
       </el-col>
     </el-row>
@@ -28,6 +47,8 @@
 <script setup name="Cache">
 import {getCache} from '@/api/monitor/cache';
 import * as echarts from 'echarts';
+import {Pane, Splitpanes} from 'splitpanes'
+import 'splitpanes/dist/splitpanes.css'
 
 const cache = ref([]);
 const commandstats = ref(null);
@@ -91,3 +112,25 @@ function getList() {
 
 getList();
 </script>
+<style lang="scss" scoped>
+.splitpanes {
+  background: linear-gradient(-45deg, #EE7752, #E73C7E, #23A6D5, #23D5AB);
+}
+
+.splitpanes__pane {
+  box-shadow: 0 0 5px rgba(0, 0, 0, .2) inset;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+}
+
+.splitpanes--vertical > .splitpanes__splitter {
+  min-width: 6px;
+  background: linear-gradient(90deg, #ccc, #111);
+}
+
+.splitpanes--horizontal > .splitpanes__splitter {
+  min-height: 6px;
+  background: linear-gradient(0deg, #ccc, #111);
+}
+</style>
