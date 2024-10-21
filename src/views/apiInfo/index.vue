524d5e4d84914e44457974a5d816848d798e1f8a
luck_baby <ranyong1209@gmail.com>
2024-10-21 18:15:32 +0800

    🎈 Perf(Optimize interface management code): 优化接口管理代码

--- src/views/apiInfo/index.vue
+++ src/views/apiInfo/index.vue
@@ -1,39 +1,40 @@
 <template>
   <div class="app-container">
     <el-form
-      :model="queryParams"
-      ref="queryRef"
-      :inline="true"
-      v-show="showSearch"
+        :model="queryParams"
+        ref="queryRef"
+        :inline="true"
+        v-show="showSearch"
     >
       <el-form-item label="用例名称" prop="apiName">
         <el-input
-          v-model="queryParams.apiName"
-          placeholder="请输入用例名称"
-          clearable
-          style="width: 200px"
-          @keyup.enter="handleQuery"
+            v-model="queryParams.apiName"
+            placeholder="请输入用例名称"
+            clearable
+            style="width: 200px"
+            @keyup.enter="handleQuery"
         />
       </el-form-item>
       <el-form-item label="状态" prop="apiStatus">
         <el-select
-          v-model="queryParams.apiStatus"
-          placeholder="状态"
-          clearable
-          style="width: 240px"
-          @keyup.enter="handleQuery"
+            v-model="queryParams.apiStatus"
+            placeholder="状态"
+            clearable
+            style="width: 240px"
+            @keyup.enter="handleQuery"
         >
           <el-option
-            v-for="dict in sys_normal_disable"
-            :key="dict.value"
-            :label="dict.label"
-            :value="dict.value"
+              v-for="dict in sys_normal_disable"
+              :key="dict.value"
+              :label="dict.label"
+              :value="dict.value"
           />
         </el-select>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" icon="Search" @click="handleQuery"
-          >搜索</el-button
+        >搜索
+        </el-button
         >
         <el-button icon="Refresh" @click="resetQuery">重置</el-button>
       </el-form-item>
@@ -41,201 +42,203 @@
     <el-row :gutter="10" class="mb8">
       <el-col :span="1.5">
         <el-button
-          type="primary"
-          plain
-          icon="Plus"
-          @click="handleAdd"
-          v-hasPermi="['apitest:apiInfo:add']"
-          >新增
+            type="primary"
+            plain
+            icon="Plus"
+            @click="handleAdd"
+            v-hasPermi="['apitest:apiInfo:add']"
+        >新增
         </el-button>
       </el-col>
       <el-col :span="1.5">
         <el-button
-          type="success"
-          plain
-          icon="Edit"
-          :disabled="single"
-          @click="handleUpdate"
-          v-hasPermi="['apitest:apiInfo:edit']"
-          >修改
+            type="success"
+            plain
+            icon="Edit"
+            :disabled="single"
+            @click="handleUpdate"
+            v-hasPermi="['apitest:apiInfo:edit']"
+        >修改
         </el-button>
       </el-col>
       <el-col :span="1.5">
         <el-button
-          type="danger"
-          plain
-          icon="Delete"
-          :disabled="multiple"
-          @click="handleDelete"
-          v-hasPermi="['apitest:apiInfo:remove']"
-          >删除
+            type="danger"
+            plain
+            icon="Delete"
+            :disabled="multiple"
+            @click="handleDelete"
+            v-hasPermi="['apitest:apiInfo:remove']"
+        >删除
         </el-button>
       </el-col>
       <el-col :span="1.5">
         <el-button
-          type="warning"
-          plain
-          icon="Download"
-          @click="handleExport"
-          v-hasPermi="['apitest:apiInfo:export']"
-          >导出
+            type="warning"
+            plain
+            icon="Download"
+            @click="handleExport"
+            v-hasPermi="['apitest:apiInfo:export']"
+        >导出
         </el-button>
       </el-col>
-      <right-toolbar v-model:showSearch="showSearch" @queryTable="getList" />
+      <right-toolbar v-model:showSearch="showSearch" @queryTable="getList"/>
     </el-row>
 
     <el-table
-      v-loading="loading"
-      :data="apiList"
-      @selection-change="handleSelectionChange"
+        v-loading="loading"
+        :data="apiList"
+        @selection-change="handleSelectionChange"
     >
-      <el-table-column type="selection" width="55" align="center" />
+      <el-table-column type="selection" width="55" align="center"/>
       <el-table-column
-        label="用例编号"
-        width="center"
-        align="center"
-        prop="apiId"
+          label="用例编号"
+          width="center"
+          align="center"
+          prop="apiId"
       />
       <el-table-column
-        label="用例名称"
-        width="center"
-        align="center"
-        prop="apiName"
+          label="用例名称"
+          width="center"
+          align="center"
+          prop="apiName"
       />
       <el-table-column
-        label="所属项目"
-        align="center"
-        prop="project.projectName"
-        :show-overflow-tooltip="true"
+          label="所属项目"
+          align="center"
+          prop="project.projectName"
+          :show-overflow-tooltip="true"
       />
       <el-table-column
-        label="请求方法"
-        align="center"
-        prop="apiMethod"
-        :show-overflow-tooltip="true"
+          label="请求方法"
+          align="center"
+          prop="apiMethod"
+          :show-overflow-tooltip="true"
       >
         <template #default="scope">
           <el-tag :type="getTagMethod(scope.row.apiMethod)">{{
-            scope.row.apiMethod
-          }}</el-tag>
+              scope.row.apiMethod
+            }}
+          </el-tag>
         </template>
       </el-table-column>
       <el-table-column
-        label="请求地址"
-        align="center"
-        prop="apiUrl"
-        :show-overflow-tooltip="true"
+          label="请求地址"
+          align="center"
+          prop="apiUrl"
+          :show-overflow-tooltip="true"
       />
       <el-table-column
-        label="优先级"
-        align="center"
-        prop="apiLevel"
-        :show-overflow-tooltip="true"
+          label="优先级"
+          align="center"
+          prop="apiLevel"
+          :show-overflow-tooltip="true"
       >
         <template #default="scope">
           <el-tag :type="getTagLevel(scope.row.apiLevel)">{{
-            scope.row.apiLevel
-          }}</el-tag>
+              scope.row.apiLevel
+            }}
+          </el-tag>
         </template>
       </el-table-column>
       <el-table-column
-        label="状态"
-        align="center"
-        prop="apiStatus"
-        :show-overflow-tooltip="true"
+          label="状态"
+          align="center"
+          prop="apiStatus"
+          :show-overflow-tooltip="true"
       >
         <template #default="scope">
           <dict-tag
-            :options="sys_normal_disable"
-            :value="scope.row.apiStatus"
+              :options="sys_normal_disable"
+              :value="scope.row.apiStatus"
           />
         </template>
       </el-table-column>
       <el-table-column
-        label="创建人"
-        width="center"
-        align="center"
-        prop="createBy"
-        :show-overflow-tooltip="true"
+          label="创建人"
+          width="center"
+          align="center"
+          prop="createBy"
+          :show-overflow-tooltip="true"
       />
       <el-table-column
-        label="创建时间"
-        width="180"
-        align="center"
-        prop="createTime"
-        :formatter="(row) => parseTime(row.createTime)"
-        :show-overflow-tooltip="true"
+          label="创建时间"
+          width="180"
+          align="center"
+          prop="createTime"
+          :formatter="(row) => parseTime(row.createTime)"
+          :show-overflow-tooltip="true"
       />
       <el-table-column
-        label="更新时间"
-        width="180"
-        align="center"
-        prop="updateTime"
-        :formatter="(row) => parseTime(row.updateTime)"
-        :show-overflow-tooltip="true"
+          label="更新时间"
+          width="180"
+          align="center"
+          prop="updateTime"
+          :formatter="(row) => parseTime(row.updateTime)"
+          :show-overflow-tooltip="true"
       />
       <el-table-column
-        label="更新人"
-        width="center"
-        align="center"
-        prop="updateBy"
-        :show-overflow-tooltip="true"
+          label="更新人"
+          width="center"
+          align="center"
+          prop="updateBy"
+          :show-overflow-tooltip="true"
       />
       <el-table-column
-        label="最后执行状态"
-        width="120"
-        align="center"
-        prop="xxx"
-        :show-overflow-tooltip="true"
+          label="最后执行状态"
+          width="120"
+          align="center"
+          prop="xxx"
+          :show-overflow-tooltip="true"
       />
       <el-table-column
-        label="最后执行时间"
-        width="180"
-        align="center"
-        prop="xxx"
-        :show-overflow-tooltip="true"
+          label="最后执行时间"
+          width="180"
+          align="center"
+          prop="xxx"
+          :show-overflow-tooltip="true"
       />
       <el-table-column
-        width="140"
-        label="操作"
-        align="center"
-        class-name="small-padding fixed-width"
+          width="140"
+          label="操作"
+          align="center"
+          class-name="small-padding fixed-width"
       >
         <template #default="scope">
           <el-tooltip content="运行" placement="top">
             <el-button
-              link
-              type="primary"
-              icon="Position"
-              @click="handleDebug(scope.row)"
-              v-hasPermi="['apitest:apiInfo:debug']"
+                link
+                type="primary"
+                icon="Position"
+                @click="handleDebug(scope.row)"
+                v-hasPermi="['apitest:apiInfo:debug']"
             ></el-button>
           </el-tooltip>
           <el-tooltip content="修改" placement="top">
             <el-button
-              link
-              type="primary"
-              icon="Edit"
-              @click="handleUpdate(scope.row)"
-              v-hasPermi="['apitest:apiInfo:edit']"
+                link
+                type="primary"
+                icon="Edit"
+                @click="handleUpdate(scope.row)"
+                v-hasPermi="['apitest:apiInfo:edit']"
             ></el-button>
           </el-tooltip>
           <el-tooltip content="复制" placement="top">
             <el-button
-              link
-              type="primary"
-              icon="Connection"
-              @click="handleUpdate(scope.row)"
-              v-hasPermi="['apitest:apiInfo:edit']"
+                link
+                type="primary"
+                icon="Connection"
+                @click="handleUpdate(scope.row)"
+                v-hasPermi="['apitest:apiInfo:edit']"
             ></el-button>
           </el-tooltip>
           <el-tooltip content="删除" placement="top">
             <el-button
-              link
-              type="primary"
-              icon="Delete"
-              @click="handleDelete(scope.row)"
-              v-hasPermi="['apitest:apiInfo:remove']"
+                link
+                type="primary"
+                icon="Delete"
+                @click="handleDelete(scope.row)"
+                v-hasPermi="['apitest:apiInfo:remove']"
             ></el-button>
           </el-tooltip>
         </template>
@@ -243,11 +246,11 @@
     </el-table>
 
     <pagination
-      v-show="total > 0"
-      :total="total"
-      v-model:page="queryParams.pageNum"
-      v-model:limit="queryParams.pageSize"
-      @pagination="getList"
+        v-show="total > 0"
+        :total="total"
+        v-model:page="queryParams.pageNum"
+        v-model:limit="queryParams.pageSize"
+        @pagination="getList"
     />
 
     <!-- 添加或修改接口目对话框 -->
@@ -266,10 +269,11 @@ import {
   testApiById,
   updateApi,
 } from "@/api/apiInfo/apiInfo";
+import {listProject} from "@/api/project/project";
 import EditApi from "./components/EditApi.vue";
 
-const { proxy } = getCurrentInstance();
-const { sys_normal_disable } = proxy.useDict("sys_normal_disable");
+const {proxy} = getCurrentInstance();
+const {sys_normal_disable} = proxy.useDict("sys_normal_disable");
 const apiList = ref([]);
 const open = ref(false);
 const loading = ref(true);
@@ -350,7 +354,7 @@ const data = reactive({
   },
   rules: {
     apiName: [
-      { required: true, message: "接口名称不能为空", trigger: "blur" },
+      {required: true, message: "接口名称不能为空", trigger: "blur"},
       {
         max: 10,
         message: "接口名称不能超过10个字符",
@@ -358,7 +362,7 @@ const data = reactive({
       },
     ],
     api_url: [
-      { required: true, message: "接口url不能为空", trigger: "blur" },
+      {required: true, message: "接口url不能为空", trigger: "blur"},
       {
         max: 512,
         message: "接口url不能超过512个字符",
@@ -366,7 +370,7 @@ const data = reactive({
       },
     ],
     api_method: [
-      { required: true, message: "接口方法不能为空", trigger: "blur" },
+      {required: true, message: "接口方法不能为空", trigger: "blur"},
       {
         max: 10,
         message: "接口方法不能超过10个字符",
@@ -374,7 +378,7 @@ const data = reactive({
       },
     ],
     api_level: [
-      { required: true, message: "接口优先级不能为空", trigger: "blur" },
+      {required: true, message: "接口优先级不能为空", trigger: "blur"},
       {
         max: 2,
         message: "接口优先级不能超过2个字符",
@@ -382,7 +386,7 @@ const data = reactive({
       },
     ],
     api_status: [
-      { required: true, message: "接口状态不能为空", trigger: "blur" },
+      {required: true, message: "接口状态不能为空", trigger: "blur"},
       {
         max: 1,
         message: "接口状态不能超过1个字符",
@@ -390,12 +394,12 @@ const data = reactive({
       },
     ],
     apiLevel: [
-      { required: true, message: "请选择机接口优先级", trigger: "blur" },
+      {required: true, message: "请选择机接口优先级", trigger: "blur"},
     ],
   },
 });
 
-const { queryParams, form, rules } = toRefs(data);
+const {queryParams, form, rules} = toRefs(data);
 
 /** 查询接口列表 */
 function getList() {
@@ -454,6 +458,7 @@ function handleSelectionChange(selection) {
 /** 新增按钮操作 */
 function handleAdd() {
   reset();
+  listProject()
   open.value = true;
   title.value = "添加接口";
 }
@@ -474,6 +479,7 @@ function handleUpdate(row) {
   reset();
   const apiId = row.apiId || ids.value;
   getApiById(apiId).then((response) => {
+    listProject()
     form.value = response.data;
     open.value = true;
     title.value = "修改接口";
@@ -521,25 +527,26 @@ function handleDelete(row) {
   const apiId = row.apiId || ids.value;
   const apiName = row.apiName || ids.value;
   proxy.$modal
-    .confirm('是否确认删除接口名称为"' + apiName + '"的数据项?')
-    .then(function () {
-      return delApi(apiId);
-    })
-    .then(() => {
-      getList();
-      proxy.$modal.msgSuccess(apiName + "删除成功");
-    })
-    .catch(() => {});
+      .confirm('是否确认删除接口名称为"' + apiName + '"的数据项?')
+      .then(function () {
+        return delApi(apiId);
+      })
+      .then(() => {
+        getList();
+        proxy.$modal.msgSuccess(apiName + "删除成功");
+      })
+      .catch(() => {
+      });
 }
 
 /** 导出按钮操作 */
 function handleExport() {
   proxy.download(
-    "",
-    {
-      ...queryParams.value,
-    },
-    `api_${new Date().getTime()}.xlsx`
+      "",
+      {
+        ...queryParams.value,
+      },
+      `api_${new Date().getTime()}.xlsx`
   );
 }
 
