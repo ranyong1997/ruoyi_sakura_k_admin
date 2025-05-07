7b0ed52db172ac99133bca5c0b1c07c57f4ece09
luck_baby <ranyong1209@gmail.com>
2025-05-07 10:31:16 +0800

    ✨ Feat(Added server operation interface): 新增服务器操作接口

--- src/api/ssh/ssh.js
+++ src/api/ssh/ssh.js
@@ -2,65 +2,112 @@ import request from '@/utils/request'
 
 const baseurl = '/ssh/'
 
-// 查询服务器列表
-export function listSsh(query) {
+// 测试SSH连接
+export function TestSsh(data) {
     return request({
-        url: baseurl + '/list',
-        method: 'get',
-        params: query
-    })
-}
-
-
-// 新增服务器
-export function addSsh(data) {
-    return request({
-        url: baseurl,
+        url: baseurl + '/connect/test',
         method: 'post',
         data: data
     })
 }
 
-// 修改服务器
-export function updateSsh(data) {
+
+// 执行SSH命令
+export function ExecuteCommand(data) {
     return request({
-        url: baseurl,
-        method: 'put',
+        url: baseurl + '/command/execute',
+        method: 'post',
         data: data
     })
 }
 
-// 删除服务器
-export function delSsh(sshId) {
+// 执行SSH脚本
+export function ExecuteScript(data) {
     return request({
-        url: baseurl + '/' + sshId,
-        method: 'delete'
-    })
-}
-
-
-// 查询服务器详细
-export function getSshById(sshId) {
-    return request({
-        url: baseurl + '/' + sshId,
-        method: 'get',
-        params: sshId
-    })
-}
-
-// 根据 ID 复制服务器
-export function copySshById(sshId) {
-    return request({
-        url: baseurl + '/copy/' + sshId,
-        method: 'post'
-    })
-}
-
-// 根据 ID 测试连接机器人
-export function testSshById(sshId) {
-    return request({
-        url: baseurl + '/' + sshId,
+        url: baseurl + '/script/execute',
         method: 'post',
-        data: sshId
+        data: data
+    })
+}
+
+// 上传文件到远程服务器
+export function UploadFile(data) {
+    return request({
+        url: baseurl + '/file/upload',
+        method: 'post',
+        data: data
+    })
+}
+
+// 从远程服务器下载文件
+export function DownloadFile(data) {
+    return request({
+        url: baseurl + '/file/download',
+        method: 'post',
+        data: data
+    })
+}
+
+// 写入文本到远程文件
+export function WriteText(data) {
+    return request({
+        url: baseurl + '/text/write',
+        method: 'post',
+        data: data
+    })
+}
+
+
+// 读取远程文件文本内容
+export function ReadText(data) {
+    return request({
+        url: baseurl + '/text/read',
+        method: 'post',
+        data: data
+    })
+}
+
+// 列出远程目录内容
+export function ListDirectory(data) {
+    return request({
+        url: baseurl + '/dir/list',
+        method: 'post',
+        data: data
+    })
+}
+
+// 创建远程目录
+export function MakeDirectory(data) {
+    return request({
+        url: baseurl + '/dir/make',
+        method: 'post',
+        data: data
+    })
+}
+
+// 删除远程文件
+export function RemoveFile(data) {
+    return request({
+        url: baseurl + '/file/remove',
+        method: 'post',
+        data: data
+    })
+}
+
+// 删除远程目录
+export function RemoveDirectory(data) {
+    return request({
+        url: baseurl + '/dir/remove',
+        method: 'post',
+        data: data
+    })
+}
+
+// 获取远程文件信息
+export function GetFileInfo(data) {
+    return request({
+        url: baseurl + '/file/info',
+        method: 'post',
+        data: data
     })
 }
